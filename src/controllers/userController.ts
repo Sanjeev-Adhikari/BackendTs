dc6f7be2c965c9f487792cc2898ce26d8b701d23
Sanjeev Adhikari <sanjeevadhikari75@gmail.com>
2024-08-30 14:45:06 +0545

    Completed register,login and werror handling

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,10 +1,15 @@
 import { Request, Response } from "express";
 import User from "../database/models/User";
+import bcrypt from 'bcryptjs'
+import jwt from 'jsonwebtoken'
+
+
 
 class AuthController{
     public static async registerUser(req:Request, res:Response):Promise<void>{
-        
-        const {username, email, password} = req.body
+        //Take input
+        const {username, email, password, role} = req.body
+        //validate input
         if(!username || !email || !password){
              res.status(400).json({
                 message: "Please fill the required fields"
@@ -12,15 +17,72 @@ class AuthController{
             return
         } 
 
+        //check if the user is already registered with the given email
+         const [userFound] = await User.findAll({
+            where : {
+                email : email
+            }
+         })
+         if(userFound){
+            res.status(401).json({
+                message : "user with that email registered already"
+            })
+            return
+         }  
+
+        //create a new user
         await User.create({
             username,
             email,
-            password
+            password : bcrypt.hashSync(password, 10),
+            role : role
         })
 
         res.status(200).json({
             message : "User registered successfully"
         })
+    }
+
+    public static async loginUser(req:Request, res:Response): Promise<void>{
+        //take input
+        const {email,password} = req.body
+        //Validate input
+        if(!email || !password){
+            res.status(400).json({
+                message : "Please provide email and password"
+            })
+            return
+        }
+        //Check if a user with the input email exists
+        const [data] = await User.findAll({
+            where : {
+                email : email
+            }
+        })
+        //if email is not found
+        if(!data){
+            res.status(400).json({
+                message : "No user found with that email"
+            })
+            return
+        }
+        //check if the password is correct or not
+        const isMatched = bcrypt.compareSync(password, data.password)
+        if(!isMatched){
+            res.status(403).json({
+                message : "Invalid Email or Password"
+            })
+            return
+        }
+        //generate token
+        const token = jwt.sign({id:data.id}, process.env.SECRET_KEY as string, {
+            expiresIn: "20d"
+        }
+        )
+        res.status(200).json({
+            message : "User Logged in successfully",
+            data : token
+        })  
     }
 }
 
--- src/routes/userRoute.ts
+++ src/routes/userRoute.ts
@@ -1,13 +1,15 @@
 import express, {Router} from 'express'
 import AuthController from '../controllers/userController'
+import errorHandler from '../services/catchAsync'
 const router:Router = express.Router()
 
 //Routes
 
 router.route("/register")
-.post(AuthController.registerUser)
+.post(errorHandler(AuthController.registerUser))
+
+router.route("/login")
+.post(errorHandler(AuthController.loginUser))
+
 //Routes end
-
-
-
 export default router
